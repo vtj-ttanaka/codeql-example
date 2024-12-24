f07c6e600b0fa1595675c6e4b0f047f090c31519
ﾀﾅｶ ﾄﾓｱｷ <86752616+vtj-ttanaka@users.noreply.github.com>
2024-12-24 17:57:09 +0900

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-protected void doGet(HttpServletRequest request, HttpServletResponse resp) {
-    String firstName = request.getParameter("firstName");
-    resp.getWriter().append("<div>");
-    resp.getWriter().append("Search for " + firstName);
-    resp.getWriter().append("</div>");
-}
